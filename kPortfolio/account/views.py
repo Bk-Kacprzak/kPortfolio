267f89e505f4b1a454762c23cb59209098977dd5
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-07-21 19:39:29 +0200

    feature: add new views

--- kPortfolio/account/views.py
+++ kPortfolio/account/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import logout, authenticate, login
-from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponseRedirect
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib import messages
 
@@ -10,36 +10,27 @@ from django.utils.encoding import force_bytes, force_text
 from django.conf import settings
 
 from .models import User
-from .forms import RegisterForm, LoginForm
+from .forms import RegisterForm, LoginForm, ForgotPasswordForm, ResetPasswordForm
 from .decorators import user_not_logged_in
-from .tokens import account_activation_token
+from .tokens import generate_token
+
+from django.utils.encoding import force_str
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.decorators import login_required
 
 
 @user_not_logged_in
-def register_request(request):
+def register(request):
     form = RegisterForm()
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
+            user.save()
             send_registration_email(request, user=user, email_to=form.cleaned_data.get('email'))
             return render(request, "account/auth/register-confirm.html")
 
     return render(request, "account/auth/register.html", {"form": form})
-
-
-def email_confirmation(request):
-    return render(request, "account/auth/login.html", {})
-
-
-@user_not_logged_in
-def forgot_request(request):
-    return render(request, "account/auth/forgot.html", {})
-
-
-def logout_request(request):
-    logout(request)
-    return HttpResponseRedirect('/account/login')
 
 
 @user_not_logged_in
@@ -49,18 +40,37 @@ def login_request(request):
         form = LoginForm(request, data=request.POST)
         if form.is_valid():
             user = form.login()
+            if not user.email_activated:
+                return render(request, 'account/auth/verify-email.html')
             if user:
                 login(request, user)
                 return redirect("/")
         else:
-            messages.error(request, message="Invalid email address or password.")
+            messages.error(request, _("Invalid email address or password."))
 
     return render(request, "account/auth/login.html", {"login_form": form})
 
 
-from django.utils.translation import gettext_lazy as _
+@user_not_logged_in
+def forgot_password(request):
+    if request.method == 'POST':
+        form = ForgotPasswordForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            user = User.objects.filter(email=email).first()
+            if not user.email_activated:
+                return render(request, 'account/auth/verify-email.html')
+            if user:
+                email_send = send_forgot_password_mail(request, user)
+                messages.success(request, _('Check out your e-mail account. We have sent reset password mail.'))
+            else:
+                messages.error(request, _('This e-mail does not exist in our service.'))
+    else:
+        form = ForgotPasswordForm()
 
-from django.core.mail import send_mail
+    return render(request, "account/auth/forgot-password.html", {
+        'form': form
+    })
 
 
 def send_registration_email(request, user, email_to):
@@ -71,22 +81,74 @@ def send_registration_email(request, user, email_to):
         'user': user,
         'domain': current_site.domain,
         'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-        'token': account_activation_token.make_token(user),
+        'token': generate_token.make_token(user),
     })
 
     text_content = 'This is an important message.'
-    # message = render_to_string("account/auth/acc-active-account.html", {
-    #     'user': user,
-    #     'domain': current_site.domain,
-    #     'uid':urlsafe_base64_encode(force_bytes(user.pk)),
-    #     'token': account_activation_token.make_token(user)
-    # })
-    send_mail(
-        email_subject, message,
-        settings.EMAIL_HOST_USER,
-        [email_to]
-    )
+    msg = EmailMultiAlternatives(email_subject, text_content, settings.DEFAULT_EMAIL_SENDER, [user.email])
+    msg.attach_alternative(message, "text/html")
+
+    return msg.send()
+
+
+def send_forgot_password_mail(request, user):
+    current_site = get_current_site(request)
+    email_subject = _('Change your Password')
+    message = render_to_string('account/auth/forgot-password-mail.html', {
+        'user': user,
+        'domain': current_site.domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': generate_token.make_token(user),
+    })
+
+    text_content = 'This is an important message.'
+    msg = EmailMultiAlternatives(email_subject, text_content, settings.DEFAULT_FROM_EMAIL, [user.email])
+    msg.attach_alternative(message, "text/html")
+    return msg.send()
 
 
 def activate(request, uidb64, token):
-    pass
+    uid = force_str(urlsafe_base64_decode(uidb64))
+    user = User.objects.get(pk=uid)
+
+    if user is not None and generate_token.check_token(user, token):
+        user.email_activated = True
+        user.save()
+        return redirect('login')
+
+    return redirect(reverse('index')) #TODO: create template if activation refused
+
+
+@user_not_logged_in
+def reset_password(request, uidb64, token):
+    uid = force_str(urlsafe_base64_decode(uidb64))
+    user = User.objects.get(pk=uid)
+
+    form = ResetPasswordForm()
+    if not user.email_activated:
+        return render(request, 'account/auth/verify-email.html')
+
+    if request.method == 'POST':
+        form = ResetPasswordForm(request.POST)
+        if not form.is_valid():
+            return render(request, "account/auth/reset-password.html", {
+                'form': form
+            })
+
+        password = form.cleaned_data['password']
+        if generate_token.check_token(user, token):
+            user.set_password(password)
+            user.save()
+            return redirect(reverse('login'))
+        else:
+            messages.error(request, _('Your token has expired'))
+
+    return render(request, "account/auth/reset-password.html", {
+        'form': form
+    })
+
+
+def logout_request(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect(reverse('index'))
