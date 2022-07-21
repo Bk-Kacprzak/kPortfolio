afe84a30167fafb6fd8e073020492f7059513653
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-07-21 19:24:20 +0200

    feature: add new url patterns

--- kPortfolio/account/urls.py
+++ kPortfolio/account/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("register/", views.register_request, name='register'),
-    path("login/", views.login_request, name='login'),
-    path("logout/", views.logout_request, name='logout'),
-    path("forgot/", views.forgot_request, name='forgot'),
-    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
-         views.activate, name='activate'),
-    path("email-confirmation/", views.email_confirmation, name='email_confirmation'),
+    path('register/', views.register_request, name='register'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('forgot/', views.forgot_password, name='forgot_password'),
+    path('reset-password/<uidb64>/<token>', views.reset_password, name='reset_password'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
