3a2edb3574bc67ac8dadc5e45437111fcc1b8f86
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-07-21 19:42:01 +0200

    fix: refactor code for readability

--- kPortfolio/portfolio/views.py
+++ kPortfolio/portfolio/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def portfolio(request):
     return render(request, "portfolio/portfolio.html", {})
 
+
+@login_required
 def portfolio_overview(request):
     return render(request, "portfolio/overview.html", {})
 
