71d19710e6318d175f12eca292cdf58a08786e5f
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-07-28 12:50:52 +0200

    feature: add Portfolio model

--- kPortfolio/portfolio/views.py
+++ kPortfolio/portfolio/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib.auth.decorators import login_required
-
+from .forms import PortfolioForm
+from .models import Portfolio
 
 @login_required
 def portfolio(request):
-    return render(request, "portfolio/portfolio.html", {})
+    form = PortfolioForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            total_cost = form.cleaned_data['total_cost']
+            portfolio = form.save(commit=False)
+            portfolio.user = request.user
+            portfolio.save()
+            return redirect(reverse('portfolio_overview'))
+
+    return render(request, "portfolio/portfolio.html", {
+        'form': form
+    })
 
 
 @login_required
 def portfolio_overview(request):
+
     return render(request, "portfolio/overview.html", {})
 
