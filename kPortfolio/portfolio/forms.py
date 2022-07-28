ac99cfc87d28a4fe3d853491e8ff86805e36fde9
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-07-28 12:50:46 +0200

    feature: add Portfolio model

---
+++ kPortfolio/portfolio/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.utils.translation import gettext_lazy as _
+import re
+
+from .models import Portfolio
+from ..utils import style_form
+
+
+class PortfolioForm(forms.ModelForm):
+    class Meta:
+        model = Portfolio
+        fields = ('name', 'total_cost')
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control form-control-lg form-control-solid',
+                'style': 'letter-spacing: 1px; color: #303030; font-weight:400'
+            }),
+            'total_cost': forms.TextInput(attrs={
+                'class': 'form-control form-control-lg form-control-solid',
+                'style': 'letter-spacing: 1px; color: #303030; font-weight:400'
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(PortfolioForm, self).__init__(*args, **kwargs)
+        # style_form(self.fields,  attrs={ 'class': 'form-control', 'style': 'letter-spacing: 1px; width: 340px;
+        # background: white; height: 40px; color: #303030; font-weight:400', })
+
+
--- kPortfolio/portfolio/models.py
+++ kPortfolio/portfolio/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from ..account.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Portfolio(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(_('Portfolio name'), max_length=255, unique=False, blank=True)
+    total_cost = models.PositiveIntegerField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+
+    class Meta:
+        verbose_name = _('Portfolio')
+        verbose_name_plural = _('Portfolios')
+
+    def __str__(self):
+        return self.name
