979596fea19919923fa1519ad6131fe0fd7802d7
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-07-28 15:37:45 +0200

    feature: Transaction Model

--- kPortfolio/asset/models.py
+++ kPortfolio/asset/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from ..portfolio.models import Portfolio
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Asset(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(_('Asset name'), max_length=50, unique=False, null=False, blank=True)
+    ticker = models.CharField(_('Ticker'), max_length=6, unique=False, null=False)
+
+    price = models.FloatField(_('Price'), null=False)
+    marketcap = models.PositiveSmallIntegerField(_('Market cap'), null=True, unique=True)
+
+    # 24h change
+    # 1w change
+    # 1m change
+
+    def __str__(self):
+        return self.name
+
+    def get_price(self):
+        return self.price
