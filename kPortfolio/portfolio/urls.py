3e49f0f1995691aa8ef68262d6ac321681eadd52
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-07-28 12:52:07 +0200

    fix: url path

--- kPortfolio/portfolio/urls.py
+++ kPortfolio/portfolio/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.portfolio, name='portfolio'),
-    path("overview", views.portfolio_overview, name='portfolio_overview'),
+    path("/overview", views.portfolio_overview, name='portfolio_overview'),
 ]
 
