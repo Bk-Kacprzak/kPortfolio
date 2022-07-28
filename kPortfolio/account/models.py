0c052a3a37d4bef0081408baf511ac568005902b
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-07-28 11:07:06 +0200

    feature: refactor User model

--- kPortfolio/account/models.py
+++ kPortfolio/account/models.py
@@ -1,52 +1,53 @@
 from django.db import models
 
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 from django.utils.translation import ugettext_lazy as _
 
-
 class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
 
     def _create_user(self, email, password, **extra_fields):
-        if not email :
-            raise ValueError("Email is required.")
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not validate_is_none(username, email):
-            return
-
-        user = self.model(username=username, email=self.normalize_email(email))
-        user.set_password(password)
-        user.save()
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, username, email, password, **extra_fields):
-        if not validate_is_none(password):
-            return
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-        user = self.create_user(username, email, password)
-        user.is_superuser = True
-        user.is_staff = True
-        user.save()
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
         return self._create_user(email, password, **extra_fields)
 
 
-class User(AbstractBaseUser, PermissionsMixin):
+class User(AbstractUser):
     id = models.AutoField(primary_key=True)
     username = None
-    first_name = models.CharField(_('first name'), max_length=255, unique=False, db_index=True)
-    email = models.EmailField(_('email address'), unique=True, db_index=True)
+    first_name = models.CharField(_('first name'), max_length=255, unique=False, blank=True)
+    email = models.EmailField(_('email address'), unique=True)
     email_activated = models.BooleanField(default=False, blank=True)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-    is_staff = True
 
     class Meta:
         verbose_name = _('user')
@@ -62,3 +63,4 @@ def validate_is_none(*args):
             raise ValidationError(f'{value} is required.')
 
     return True
+
