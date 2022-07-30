06598ea62830fcf29824a541ffd4c1ec951b0876
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-07-30 12:09:01 +0200

    fix: change model field types

--- kPortfolio/transaction/models.py
+++ kPortfolio/transaction/models.py
@@ -12,11 +12,10 @@ class TransactionManager(models.Manager):
 class Transaction(models.Model):
     # id = models.AutoField(primary_key=True)
     portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, null=False, blank=False)
-    asset = models.ForeignKey(UserAsset, on_delete=models.PROTECT, null=False, blank=False)
-
+    asset = models.ForeignKey(UserAsset, on_delete=models.CASCADE, null=False, blank=False)
     price = models.FloatField(_('Price'), null=False)
-    cost = models.PositiveIntegerField(blank=True, null=True)
-    amount = models.PositiveIntegerField(blank=True, null=True)
+    cost = models.FloatField(blank=True, null=True)
+    amount = models.FloatField(blank=True, null=True)
     date = models.DateField(_('Transaction date'), blank=True, null=True, default=date.today().strftime('%m/%d/%y'))
 
     type = models.CharField(_('Transaction type'), max_length=5
