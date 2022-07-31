d7e028d846495c200a8592799639ae523937bd81
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-07-31 13:32:29 +0200

    feature: add new fields to Asset model

--- kPortfolio/asset/models.py
+++ kPortfolio/asset/models.py
@@ -6,16 +6,26 @@ import numpy as np
 
 class AssetManager(models.Manager):
     def get_coins_names(self, amount):
-        return self.filter(marketcap__lte=amount)
+        return self.filter(market_cap_rank__lte=amount)
 
 
 class Asset(models.Model):
-    id = models.AutoField(primary_key=True)
+    name_id = models.CharField(_("Name id"), max_length=128, unique=True, db_index=True, null=False)
     name = models.CharField(_('Asset name'), max_length=50, unique=False, null=False, blank=True)
     ticker = models.CharField(_('Ticker'), max_length=6, unique=False, null=False)
-
+    image = models.URLField(unique=False, null=True)
     price = models.FloatField(_('Price'), null=False)
-    marketcap = models.PositiveSmallIntegerField(_('Market cap'), null=True, unique=True)
+    market_cap_rank = models.PositiveSmallIntegerField(_('Market cap rank'), null=True, unique=False, db_index=True)
+    market_cap = models.BigIntegerField(_("Market Cap"), null=True, blank=True)
+    total_volume = models.BigIntegerField(_("Total volume"), null=True, blank=True)
+    high_24h = models.FloatField(_("High 24 h"), null=True, blank=True)
+    low_24h = models.FloatField(_("Low 24 h"), null=True, blank=True)
+    price_change_24h = models.FloatField(_("Price change 24h"), blank=True, null=True)
+    price_change_percentage_24h = models.FloatField(_("Price change percentage 24h"), blank=True, null=True)
+    circulating_supply = models.BigIntegerField(_("Circulating supply"), blank=True, null=True)
+    total_supply = models.BigIntegerField(_("Total supply"), blank=True, null=True)
+    max_supply = models.BigIntegerField(_("Max supply"), blank=True, null=True)
+    ath = models.FloatField(_("All time high"), blank=True, null=True)
 
     # 24h change
     # 1w change
